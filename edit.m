05dd133adc6576de1cab63762181b34b7b576a89
woclass <5158738+inkydragon@users.noreply.github.com>
2021-07-16 19:15:49 +0800

    ignore unused func

--- edit.m
+++ edit.m
@@ -8,14 +8,14 @@ fun merk f  =  if f <> "" then script := f ^ ".sml" else ()
 (* val system  =      System.system   (* 0.93  *)  *)
    val system  =  OS.Process.system   (* 109.2 *)
 
-fun edit fileName  =
+(* fun edit fileName  =
 (  system ("cp " ^ fileName ^ " @tmp");
    system ("vi " ^ fileName);
    if  system ("cmp -s @tmp " ^ fileName) = 0wx1  then  use fileName  else  ()
-)
+) *)
 
 fun U f  =  ( merk f;  use  (!script) )
 
-fun E f  =  ( merk f;  edit (!script) )
+(* fun E f  =  ( merk f;  edit (!script) )
 
-fun A ()  =  edit "all.sml"
+fun A ()  =  edit "all.sml" *)
